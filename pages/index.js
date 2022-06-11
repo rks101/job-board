3ac75c5921bc5fdce2d13b04b1355bc624309934
rks101 <rajendra.kr.solanki@gmail.com>
2022-06-11 20:10:32 +0530

    added Job listing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import prisma from 'lib/prisma'
+import { getJobs } from 'lib/data.js'
+import Jobs from 'components/Jobs'
 
-export default function Home() {
-  return (<p>Hello</p>
+export default function Home({ jobs }) {
+  return (
+    <div className='mt-10'>
+      <div className='text-center p-4 m-4'>
+        <h2 className='mb-10 text-4xl font-bold'>Find a job!</h2>
+      </div>
+      <Jobs jobs={jobs} />
+    </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  let jobs = await getJobs(prisma)
+	jobs = JSON.parse(JSON.stringify(jobs))
+
+  return {
+    props: {
+      jobs,
+    },
+  }
 }
